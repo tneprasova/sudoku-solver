fb4daf1f62be380fa29f0664c799d5392a7f7ce9
Tereza Neprašová <neprater@fit.cvut.cz>
2022-11-20 17:24:27 +0100

    Palindrome

---
+++ hw_racket/palindrome.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define (myLen lst)
+  (if (null? lst)
+      0
+      (+ 1 (myLen(cdr lst)))))
+
+(define (removeEnd lst)
+  (if (<= (myLen lst) 1)
+      null
+      (cons (car lst) (removeEnd (cdr lst)))))
+
+(define (removeEnds lst)
+  (if (<= (myLen lst) 2)
+      null
+      (removeEnd (cdr lst))))
+
+(define (myAppend x lst)
+  (if (null? lst)
+      (cons x null)
+      (cons (car lst) (myAppend x (cdr lst)))))
+
+(define (myRev lst)
+  (if (null? lst)
+      null
+      (myAppend (car lst) (myRev (cdr lst)))))
+
+(define (is-palindrome? p)
+  (if (null? p)
+      true
+      (equal? (removeEnds p) (myRev (removeEnds p)))))
