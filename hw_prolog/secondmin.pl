86ba7cf6cd78c28d8e055306e4d73b100f4a1865
Tereza Neprašová <neprater@fit.cvut.cz>
2022-12-26 22:38:35 +0100

    Rollback

--- hw_prolog/secondmin.pl
+++ hw_prolog/secondmin.pl
@@ -6,22 +6,19 @@ my_delete([H|T], E, Res) :-
 my_delete([H|T], E, [H|Res]) :-
     my_delete(T, E, Res).
 
-find_second_min([], 1.0Inf, 1.0Inf) :- !.
-find_second_min([H|T], H, R2) :-
-    find_second_min(T, R1, R2),
-    H =< R1, !.
-find_second_min([H|T], R1, H) :-
-    find_second_min(T, R1, R2),
-    H > R1,
-    H =< R2, !.
-find_second_min([H|T], R1, R2) :-
-    find_second_min(T, R1, R2),
-    H > R1.
+find_min([A], A) :- !.
+find_min([H|T], H) :-
+    find_min(T, R),
+    H =< R, !.
+find_min([H|T], R) :-
+    find_min(T, R),
+    H > R.
 
 %without_second_min(+Lst, -Res)
 without_second_min([], _) :- fail, !.
 without_second_min([_], _) :- fail, !.
 without_second_min(Lst, Res) :-
-    find_second_min(Lst, _, B),
-    B \= 1.0Inf,
+    find_min(Lst, A),
+    my_delete(Lst, A, Tmp),
+    find_min(Tmp, B),
     my_delete(Lst, B, Res).
