73f505c493613ffb7525395bf31af2215e0148cc
Tereza Neprašová <neprater@fit.cvut.cz>
2022-12-28 19:09:19 +0100

    Redo row checking, introduce transpose

--- sudoku.pl
+++ sudoku.pl
@@ -1,24 +1,50 @@
-is_row_unique(Row) :-
+% Check unique values in a list
+are_unique(Row) :-
     list_to_set(Row, Set),
     length(Row, A),
-    length(Set, B),
-    A = B.
+    length(Set, A).
 
-% Check dimensions and unique values in the rows
-checkRows(S) :-
-    length(S, L),
-    checkRows(S, L).
-checkRows([R], L1) :- 
-    is_row_unique(R),
-    length(R, L2),
-    L1 = L2,
-    maplist(between(1, L1), R).
-checkRows([R|T], L1) :-
-    is_row_unique(R),
-    length(R, L2),
-    L1 = L2,
-    checkRows(T, L1),
-    maplist(between(1, L1), R).
+% Set values, 0 or _ is considered to be an empty field
+mapValue(_, V, V) :- V \= 0, !.
+mapValue(Max, _, Res) :-
+    between(1, Max, Res).
 
-sudoku(S):-
-    checkRows(S).
+mapValues([], _, _).
+mapValues([T|H], [R|Rs], Max) :-
+    maplist(mapValue(Max), T, R),
+    mapValues(H, Rs, Max).
+
+% Transpose a matrix
+transpose([M|Ms], Res) :-
+    transpose(M, [M|Ms], Res).
+
+transpose([], _, []).
+transpose([_|Ms], Matrix, [Row|Rs]) :-
+    first_column_to_row(Matrix, Row, Matrix_minus_column),
+    transpose(Ms, Matrix_minus_column, Rs).
+
+first_column_to_row([], [], []).
+first_column_to_row([[F|RestOfRow]|RestOfRows], [F|Fs], [RestOfRow|Rs]) :-
+    first_column_to_row(RestOfRows, Fs, Rs).
+
+
+
+% The main function
+sudoku(Rows, Res):-
+    % Check the dimensions (is a square)
+    maplist(same_length(Rows), Rows),
+    
+    % Try to map values
+    length(Rows, Len),
+    length(NewRows, Len),
+    mapValues(Rows, NewRows, Len),
+
+    % Check, whether the values were correctly mapped
+    maplist(are_unique, NewRows),
+    transpose(NewRows, Columns),
+    maplist(are_unique, Columns),
+    transpose(Columns, Res),
+
+    % Output
+    writeln(">>"),
+    maplist(writeln, Res).
